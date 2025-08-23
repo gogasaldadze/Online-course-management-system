2be67573d74b6cba96276d0475963b27acebf4e5
giorgi <saldadzegoga@gmail.com>
2025-08-23 20:08:47 +0400

    common stuff

---
+++ common/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+import uuid
+
+
+class AbstractModel(models.Model):
+
+    uuid = models.UUIDField(
+        default=uuid.uuid4, unique=True, editable=False, db_index=True
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+        ordering = ["-created_at"]
